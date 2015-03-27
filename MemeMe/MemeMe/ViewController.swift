d56fb9136320926a007b9840933b07c474687d86
Kinan T <kinan.t@gmail.com>
2015-03-26 21:18:24 -0600

    Finished basic UI Set Up.

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        colorPick.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        // colorPick.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
