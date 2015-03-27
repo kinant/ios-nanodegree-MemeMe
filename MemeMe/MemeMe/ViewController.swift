b27f2bbe92bc30a07570d0372128ea5a5986c326
Kinan T <kinan.t@gmail.com>
2015-03-26 20:28:00 -0600

    Added Item Bar Buttons. Will look on how to position them next.

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorPick: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        colorPick.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
